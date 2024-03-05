06759037c095a090a9cf324937fa1d64cec6a517
Taylor Blanton <matthewtblanton@gmail.com>
2024-03-05 15:11:14 -0600

    refactor to use varnish, rhf, and server actions (#6)
    Addresses https://github.com/allenai/varnish/issues/711
    
    We didn't have varnish in here. Now we do! 
    
    Stole a lot from Jon's updates to the skiff-template but put a NextJS
    twist on it by using server actions.
    
    Those of you that have worked with varnish and NextJS: could you let me
    know if you see anything missing?
    
    
    ![image](https://github.com/allenai/skiff-template-nextjs/assets/2118649/7ba7e328-e9a3-46d4-a74c-43b319874eed)

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,82 +1,16 @@
-import styles from './page.module.css'
-import Header from './components/header'
-import Footer from './components/footer';
-import Main from './components/main';
+import { Typography } from '@mui/material';
 
-interface SearchParams {
-  question?: string;
-  choices?: string[];
-}
+import QuestionAndAnswer from './components/QuestionAndAnswer';
 
-interface Props {
-  searchParams: SearchParams;
-}
-
-interface Answer {
-  answer: string;
-  score: number;
-}
-
-export default async function Home({ searchParams }: Props) {
-  let answer: Answer | undefined = undefined;
-  if (searchParams.question && searchParams.choices) {
-    const origin = process.env.API_ORIGIN ?? 'http://api:8000';
-    const resp = await fetch(`${origin}/api/solve`, {
-      method: 'POST',
-      body: JSON.stringify(searchParams),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    if (!resp.ok) {
-      throw new Error(`API request failed: ${resp.status}: ${resp.statusText}`)
-    }
-    answer = await resp.json()
-  }
-
-  const nf = Intl.NumberFormat('en-US', { style: 'percent', maximumFractionDigits: 2 })
-  return (
-    <>
-      <Header />
-      <Main>
-        <h1 className={styles.title}>Skiff NextJS Template</h1>
-        <p>This is an example Skiff application that uses <a href="https://nextjs.org">NextJS</a>.</p>
-        <form method="get" action="/" className={styles.form}>
-          <div className={styles.row}>
-            <label htmlFor="question" className={styles.label}>Question:</label>
-            <textarea
-                id="question"
-                name="question"
-                placeholder="Enter a question…"
-                rows={8}
-                className={styles.text}
-                defaultValue={searchParams.question} />
-          </div>
-          <div className={styles.row}>
-            <label htmlFor="choice1" className={styles.label}>First Choice:</label>
-            <input
-              type="text"
-              id="choice1"
-              name="choices"
-              placeholder="Enter first choice…"
-              className={styles.text}
-              defaultValue={searchParams.choices ? searchParams.choices[0] : ''} />
-          </div>
-          <div className={styles.row}>
-            <label htmlFor="choice2" className={styles.label}>Second Choice:</label>
-            <input
-              type="text"
-              id="choice2"
-              name="choices"
-              placeholder="Enter first choice…"
-              className={styles.text}
-              defaultValue={searchParams.choices ? searchParams.choices[1] : ''} />
-          </div>
-          <div><input type="submit" value="Submit" className={styles.button} /></div>
-        </form>
-        {answer && (
-          <p>The answer is: <strong>{answer.answer}</strong> ({nf.format(answer.score)} confidence)</p>
-        )}
-      </Main>
-      <Footer />
-    </>
-  );
+export default function Home() {
+    return (
+        <>
+            <Typography variant="h1">Skiff NextJS Template</Typography>
+            <Typography sx={{ marginBottom: 2 }} component="p">
+                This is an example Skiff application that uses{' '}
+                <a href="https://nextjs.org">NextJS</a>.
+            </Typography>
+            <QuestionAndAnswer />
+        </>
+    );
 }

8c5981fe4f141e983b9ec42aa443a0e16541a260
Reece <41651655+radamson@users.noreply.github.com>
2026-01-22 14:34:06 -0800

    Preinstall often used packages

--- packages/autodiscovery_modal/src/autodiscovery_modal/__init__.py
+++ packages/autodiscovery_modal/src/autodiscovery_modal/__init__.py
@@ -1,11 +1,17 @@
 """Modal-adjacent helpers for running code execution workloads."""
 
-from .ipython_session import ModalIPythonBackend, lookup_run_ipython_cell, run_ipython_cell
+from .ipython_session import (
+    ModalIPythonBackend,
+    build_sandbox_image,
+    lookup_run_ipython_cell,
+    run_ipython_cell,
+)
 from .sandbox_backend import ModalSandboxIPythonBackend
 
 __all__ = [
     "ModalIPythonBackend",
     "ModalSandboxIPythonBackend",
+    "build_sandbox_image",
     "lookup_run_ipython_cell",
     "run_ipython_cell",
 ]

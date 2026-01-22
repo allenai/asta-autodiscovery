4cb4fd343b6bf55681e0f8ae4113d89cee63298b
Paul Sayre <pauls@allenai.org>
2026-01-22 14:50:06 -0800

    Add sections to home area

---
+++ ui/src/app/runs/components/RunSummary.tsx
@@ -0,0 +1,90 @@
+import { styled } from '@mui/material';
+import IconHub from '@mui/icons-material/HubOutlined';
+import IconError from '@mui/icons-material/Error';
+import { useState } from 'react';
+
+import { Run } from '@/types/Run';
+import Link from 'next/link';
+
+export type RunSummaryProps = {
+    run: Run;
+    startExpanded?: boolean;
+};
+
+export const RunSummary = ({ run, startExpanded }: RunSummaryProps) => {
+    const { id, name } = run;
+    const status = run.details?.status ?? 'unknown';
+
+    const [isExpanded, setIsExpanded] = useState(startExpanded ?? false);
+
+    return (
+        <Layout>
+            <LayoutIcon>
+                <IconWrapper onClick={() => setIsExpanded(!isExpanded)}>
+                    <RunIcon status={status} />
+                </IconWrapper>
+            </LayoutIcon>
+            <LayoutContent>
+                <TitleLink href={`/runs/${id}`} passHref>
+                    <Title>{name}</Title>
+                </TitleLink>
+                {isExpanded && <>EXPANDED</>}
+            </LayoutContent>
+        </Layout>
+    );
+};
+
+const Layout = styled('div')(({ theme }) => ({
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: theme.spacing(1),
+    padding: theme.spacing(2),
+    display: 'grid',
+    gridTemplateColumns: 'auto 1fr',
+    gap: theme.spacing(1),
+}));
+
+const LayoutIcon = styled('div')(({ theme }) => ({}));
+
+const LayoutContent = styled('div')(({ theme }) => ({}));
+
+const TitleLink = styled(Link)(({ theme }) => ({
+    textDecoration: 'none',
+    cursor: 'pointer',
+    '&:hover': {
+        textDecoration: 'underline',
+        opacity: 0.8,
+    },
+}));
+
+const Title = styled('h3')(({ theme }) => ({
+    color: '#9FEAD1',
+    margin: 0,
+    fontSize: '1.25rem',
+    lineHeight: 1.5,
+    fontWeight: 600,
+    marginTop: '-.25em',
+}));
+
+const IconWrapper = styled('div')(({ theme }) => ({
+    width: '20px',
+    height: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+}));
+
+const RunIcon = ({ status }: { status: string }) => {
+    const sharedProps = {
+        sx: {
+            height: '20px',
+            width: '20px',
+        },
+    };
+    switch (status) {
+        case 'failed':
+            return <IconError {...sharedProps} />;
+        default:
+            return <IconHub {...sharedProps} />;
+    }
+};
